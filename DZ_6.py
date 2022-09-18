433a58f39caca9566a9028e91165d039acd8ad90
I11ay <pro100otec131@gmail.com>
2022-09-19 02:49:55 +0300

    Create DZ_6.py
    Proverka

---
+++ DZ_6.py
@@ -0,0 +1,63 @@
+"""
+
+                        Классаная работа
+Первое задание:
+c = int(input())
+N = int(input())
+b = 0
+while c != N:
+    c += 1
+    b += c
+    print(b)
+
+Второе задание:
+A = int(input())
+B = int(input())
+c = 0
+while A < B:
+    A += 1
+    if A %2 == 0:
+        c += A
+        print(c)
+
+
+Третье Задание:
+Четвертое задание:
+
+Домашняя работа
+N = int(input())
+c = 0
+
+while c != N:
+    N -= 1
+    print("*" * N)
+
+N = int(input())
+c = 0
+while c != N:
+    c += 1
+    print("*" * c)
+
+N = int(input())
+c = 0
+b = -1
+while c != N:
+    N -= 1
+    b += 1
+    print(" " * b + N * "*")
+
+N = int(input())
+c = 0
+b = N
+while c != N:
+    c += 1
+    b -= 1
+    print(" " * b + c * "*")
+"""
+A = int(input())
+B = int(input())
+c = B - 2
+while A != B:
+    c += A
+    print(c)
+    print(c)
